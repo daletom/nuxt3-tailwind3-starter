24415a4ef0b9b778278d54f48dd46c375861827b
Tom Dale <thomas.dale2@gmail.com>
2022-06-29 20:52:42 -0700

    Update TheHeader.vue

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,17 +1,17 @@
 <template>
 <Popover>
-          <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+          <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
             <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
               <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                 <div class="flex items-center justify-between w-full md:w-auto">
                   <a href="/">
                     <span class="sr-only">Workflow</span>
-                    <img alt="Workflow" class="h-8 w-auto sm:h-10" src="https://tom.imgix.net/imgix-presskit.pdf?page=3&auto=format&w=100" />
+                    <img alt="Workflow" class="w-auto h-8 sm:h-10" src="https://tom.imgix.net/imgix-presskit.pdf?page=3&auto=format&w=100" />
                   </a>
-                  <div class="-mr-2 flex items-center md:hidden">
-                    <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <div class="flex items-center -mr-2 md:hidden">
+                    <PopoverButton class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span class="sr-only">Open main menu</span>
-                      <MenuIcon class="h-6 w-6" aria-hidden="true" />
+                      <MenuIcon class="w-6 h-6" aria-hidden="true" />
                     </PopoverButton>
                   </div>
                 </div>
@@ -22,22 +22,22 @@
             </nav>
           </div>
 
-          <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-            <PopoverPanel focus class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-              <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div class="px-5 pt-4 flex items-center justify-between">
+          <transition enter-active-class="duration-150 ease-out" enter-from-class="scale-95 opacity-0" enter-to-class="scale-100 opacity-100" leave-active-class="duration-100 ease-in" leave-from-class="scale-100 opacity-100" leave-to-class="scale-95 opacity-0">
+            <PopoverPanel focus class="absolute inset-x-0 top-0 z-10 p-2 transition origin-top-right transform md:hidden">
+              <div class="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+                <div class="flex items-center justify-between px-5 pt-4">
                   <div>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
+                    <img class="w-auto h-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
                   </div>
                   <div class="-mr-2">
-                    <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <PopoverButton class="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span class="sr-only">Close main menu</span>
-                      <XIcon class="h-6 w-6" aria-hidden="true" />
+                      <XIcon class="w-6 h-6" aria-hidden="true" />
                     </PopoverButton>
                   </div>
                 </div>
                 <div class="px-2 pt-2 pb-3 space-y-1">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="block px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
                 </div>
                 
               </div>
@@ -51,9 +51,9 @@
   import { MenuIcon, XIcon } from '@heroicons/vue/outline'
 
 const navigation = [
-  { name: 'Javascript', href: '/javascript' },
-  { name: 'React', href: '/react' },
-  { name: 'VueJS', href: '/vuejs' },
+  { name: 'Javascript', href: '#' },
+  { name: 'React', href: '#' },
+  { name: 'VueJS', href: '#' },
   { name: 'Company', href: '#' },
 ]
 </script>
