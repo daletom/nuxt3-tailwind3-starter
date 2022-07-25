06ee0a144eab80a4256e15d66b95067ec0a18ae9
Tom Dale <thomas.dale2@gmail.com>
2022-07-25 14:53:53 -0700

    update styling add more content

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@
       </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <img class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://ix-www.imgix.net/press/imgix-unsplash.jpeg?w=1200&auto=format,compress&cs=srgb" alt="" />
+      <img class="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://ix-www.imgix.net/press/press-leadership-podcast.jpg?w=1200&auto=format,compress&cs=srgb" alt="" />
     </div>
   </div>
   <!-- Sections -->
@@ -47,46 +47,26 @@
       <div class="grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none">
         <div v-for="post in posts" :key="post.title" class="flex flex-col overflow-hidden rounded-lg shadow-lg">
           <div class="flex-shrink-0">
+            <nuxt-link :to="post.href" class="block mt-2">
             <img class="object-cover w-full h-48" :src="post.imageUrl" alt="" />
+            </nuxt-link>
           </div>
           <div class="flex flex-col justify-between flex-1 p-6 bg-white">
+            <nuxt-link :to="post.href" class="block mt-2">
             <div class="flex-1">
               <p class="text-sm font-medium text-indigo-600">
-                <a :href="post.category.href" class="hover:underline">
+                <a class="hover:underline">
                   {{ post.category.name }}
                 </a>
               </p>
-              <a :href="post.href" class="block mt-2">
                 <p class="text-xl font-semibold text-gray-900">
                   {{ post.title }}
                 </p>
                 <p class="mt-3 text-base text-gray-500">
                   {{ post.description }}
                 </p>
-              </a>
             </div>
-            <div class="flex items-center mt-6">
-              <div class="flex-shrink-0">
-                <a :href="post.author.href">
-                  <span class="sr-only">{{ post.author.name }}</span>
-                  <img class="w-10 h-10 rounded-full" :src="post.author.imageUrl" alt="" />
-                </a>
-              </div>
-              <div class="ml-3">
-                <p class="text-sm font-medium text-gray-900">
-                  <a :href="post.author.href" class="hover:underline">
-                    {{ post.author.name }}
-                  </a>
-                </p>
-                <div class="flex space-x-1 text-sm text-gray-500">
-                  <time :datetime="post.datetime">
-                    {{ post.date }}
-                  </time>
-                  <span aria-hidden="true"> &middot; </span>
-                  <span> {{ post.readingTime }} read </span>
-                </div>
-              </div>
-            </div>
+            </nuxt-link>
           </div>
         </div>
       </div>
@@ -101,56 +81,35 @@ const posts = [
   {
     title: 'Video Demo: HLS vs MP4',
     href: 'https://hlsvsmp4-vue.vercel.app/',
-    category: { name: 'Article', href: '#' },
+    category: { name: 'Video Demo', href: '#' },
     description:
       'This app is used to showcase the user experience of playing an mp4 vs an HLS video with imgix. To test this, right-click on the browser and select "inspect", click the Network tab, change the speed to Fast 3G, and make sure the "disable cache" button next to it is selected.',
     date: 'Mar 16, 2020',
     datetime: '2020-03-16',
     imageUrl:
-      'https://source.unsplash.com/random/700x700',
-    readingTime: '6 min',
-    author: {
-      name: 'Roel Aufderehar',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
+      'https://tom.imgix.net/hlsvsmp4.png',
   },
   {
     title: 'How to use Srcset values with IX React',
     href: '/srcset',
-    category: { name: 'Video', href: '#' },
+    category: { name: 'Code Example', href: '#' },
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
     date: 'Mar 10, 2020',
     datetime: '2020-03-10',
     imageUrl:
-      'https://source.unsplash.com/random/699x699',
-    readingTime: '4 min',
-    author: {
-      name: 'Brenna Goyette',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
+      'https://assets.imgix.net/blog/device_srcset.pdf?bg=fff&fm=png8&w=992&colorquant=60',
   },
   {
-    title: 'Improve your customer experience',
-    href: '#',
-    category: { name: 'Case Study', href: '#' },
+    title: 'Using Imgix Management API',
+    href: '/ixmgmtjs',
+    category: { name: 'Code Example', href: '#' },
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
     date: 'Feb 12, 2020',
     datetime: '2020-02-12',
     imageUrl:
-      'https://source.unsplash.com/random/698x698',
-    readingTime: '11 min',
-    author: {
-      name: 'Daniela Metz',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
+      'https://assets.imgix.net/docs/setup/image-manager/Tag+Search.png?auto=format&w=800&usm=15',
   },
 ]
 
